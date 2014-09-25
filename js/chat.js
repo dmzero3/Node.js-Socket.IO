9f44dfdc6b5201faca0da4d8983bcc6bd00b71ae
matsuura_dai <matsuura_dai@cyberagent.co.jp>
2014-09-25 20:01:08 +0900

    add comment

--- js/chat.js
+++ js/chat.js
@@ -8,8 +8,10 @@ $(function(){
             alert('テキストが入力されてません。');
             return;
         }
+        //サーバーに入力したデータを送信
         socket.emit('text', data);
 
+        //自分発言用の吹き出し追加
         appendItem(data, true);
         $inputData.val('');
 
@@ -26,7 +28,9 @@ $(function(){
         $('.js_push_data').append($item);
     }
 
+    //サーバーからデータを受け取ったとき
     socket.on('text', function(data){
+        //他人発言用の吹き出し追加
         appendItem(data);
     });
 });
--- js/server_chat.js
+++ js/server_chat.js
@@ -1,14 +1,16 @@
 var Server = require('socket.io');
+//Socket.ioのインスタンス作成
 var io = new Server();
-
 //var io = require('socket.io')();
 
+//クライアントから接続があったとき
 io.sockets.on('connection', function(socket){
     //console.log('connected!!');
 
     socket.on('text', function(data){
         console.log(data);
 
+        //サーバー側からメッセージを送信する    
         socket.broadcast.emit('text', data);
     });
 });
